8276b2b21a70fe133a7e74495341c3973e18c2bf
JR Mishra <jrmnitdgp@gmail.com>
2020-10-04 23:15:35 +0530

    [JR Mishra] Add. Project Initialized

---
+++ AddNumber.cs
@@ -0,0 +1,25 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogProject
+{
+    class AddNumber
+    {
+        NLog nLog = new NLog();
+        public int Sum(int firstNumber , int secondNumber)
+        {
+            if(firstNumber ==0 || secondNumber ==0)
+            {
+                nLog.LogDebug("Debug Unsuccessfull : Sum()");
+                nLog.LogError("Expecting null values");
+                nLog.LogWarn("a or b should not equal to 0");
+            }
+            int sum = firstNumber + secondNumber;
+            nLog.LogDebug("Debug Successfull : Sum()");
+            nLog.LogInfo("Sum Method passed, Result " + sum);
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NLog;
+
+namespace NLogProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logging Program");
+
+            AddNumber sumOfTwo = new AddNumber();
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            int ans = sumOfTwo.Sum(a, b);
+            Console.WriteLine("sum : "+ans);
+
+            return;
+        }
+    }
+}
